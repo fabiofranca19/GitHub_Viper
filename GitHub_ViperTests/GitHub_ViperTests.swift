649673e66981f1fc22591a01c16e9188a9a34cef
Fábio França <fabiotibete@gmail.com>
2020-09-08 12:09:13 -0300

    Adding Xcode Project

---
+++ GitHub_ViperTests/GitHub_ViperTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_ViperTests.swift
+//  GitHub_ViperTests
+//
+//  Created by Fábio França on 08/09/20.
+//  Copyright © 2020 com.fabiofranca. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Viper
+
+class GitHub_ViperTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
